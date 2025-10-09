2d9dac7011fb3a39ed778d21eadf01e4ddcb0610
Vimal_Shady <144231334+Vimal-Shady@users.noreply.github.com>
2025-10-09 12:42:15 +0530

    mergenetworks

--- set-1/mergenetworks.py
+++ set-1/mergenetworks.py
@@ -1,22 +1,25 @@
-from collections import defaultdict,deque,Counter
-
-def f(n,x,nde):
-    
-
+from collections import deque
+def f(n,x,r):
+    q=deque()
+    q.append(r)
+    l=-1
+    vis=set()
+    while q:
+        k=len(q)
+        for _ in range(k):
+            nde=q.popleft()
+            if nde in vis: continue
+            vis.add(nde)
+            for i in x[nde]: 
+                if i in vis: continue
+                q.append(i)
+        l+=1
+    return l
 def merge(n1,af,at,n2,bf,bt):
     x=[[] for i in range(n1+1)]
     y=[[] for i in range(n2+1)]
     for i,j in enumerate(af): x[j].append(at[i]);x[at[i]].append(j)
     for i,j in enumerate(bf): y[j].append(bt[i]);y[bt[i]].append(j)
-    l=[f(n1,x,i) for i in range(1,n1+1)]
-    print(l)
-    r=[f(n2,y,i) for i in range(1,n2+1)]
-    print(r)
-
-
-
-
-
-
-
-merge(3,[1,1],[2,3],3,[1,2],[2,3])
+    net1=min(f(n1,x,i) for i in range(1,n1+1))
+    net2=min(f(n2,y,i) for i in range(1,n2+1))
+    return net1+net2+1
