af2db3cf789b89f35c1ff9ac6163116c3c32cdb0
Vimal_Shady <144231334+Vimal-Shady@users.noreply.github.com>
2025-10-09 18:52:48 +0530

    optimal

--- set-3/mergenetworks.py
+++ set-3/mergenetworks.py
@@ -1,28 +1,46 @@
 from collections import deque
+# O(N^2)
+# def f(n,x,r):
+#     q=deque()
+#     q.append(r)
+#     l=-1
+#     vis=set()
+#     while q:
+#         k=len(q)
+#         for _ in range(k):
+#             nde=q.popleft()
+#             if nde in vis: continue
+#             vis.add(nde)
+#             for i in x[nde]: 
+#                 if i in vis: continue
+#                 q.append(i)
+#         l+=1
+#     return l
+
 def f(n,x,r):
-    q=deque()
-    q.append(r)
-    l=-1
+    q=deque([(r,0)])
     vis=set()
+    fr,mxd=r,0
     while q:
-        k=len(q)
-        for _ in range(k):
-            nde=q.popleft()
-            if nde in vis: continue
-            vis.add(nde)
-            for i in x[nde]: 
-                if i in vis or i==nde: continue
-                q.append(i)
-        l+=1
-    return l
+        nde,d=q.popleft()
+        if d>mxd: fr=nde;mxd=d
+        for i in x[nde]:
+            if i in vis: continue
+            vis.add(i)
+            q.append((i,d+1))
+    return fr,mxd
+def k(n,x):
+    a,ad=f(n,x,2)
+    b,bd=f(n,x,a)
+    return (bd+1)//2
+
 def merge(n1,af,at,n2,bf,bt):
     x=[[] for i in range(n1+1)]
     y=[[] for i in range(n2+1)]
     for i,j in enumerate(af): x[j].append(at[i]);x[at[i]].append(j)
     for i,j in enumerate(bf): y[j].append(bt[i]);y[bt[i]].append(j)
-    net1=min(f(n1,x,i) for i in range(1,n1+1))
-    net2=min(f(n2,y,i) for i in range(1,n2+1))
+    net1=k(n1,x)
+    net2=k(n2,y)
     return net1+net2+1
 
 print(merge(3,[1,1],[2,3],3,[1,2],[2,3]))
-print(merge(4,[1,1,1],[2,3,4],2,[1],[2]))
